1065327ed7827bc50a36049f7c14e354a5f4a313
Maxim <maksim.pudeev@bk.ru>
2023-04-14 15:53:45 +0300

    feat: add count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,11 +14,40 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+function countCats(matrix) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+
+  let count = 0;
+  for(let i = 0; i < matrix.length; i++) {
+    // console.log(countCats[i]);
+    let point = '^^';
+    for(let j = 0; j < matrix[i].length; j++) {
+      if(matrix[i][j] === point) {
+        count++;
+      }
+    }
+  }
+  return count;
+
+
 }
 
 module.exports = {
   countCats
 };
+
+
+// let countCats = [[0, 1, '^^'],[0, '^^', 2],['^^', 1, 2]];
+
+// let count = 0;
+// for(let i = 0; i < countCats.length; i++) {
+//   // console.log(countCats[i]);
+//   let point = '^^';
+//   for(let j = 0; j < countCats[i].length; j++) {
+//     if(countCats[i][j] === point) {
+//       count++;
+//     }
+//   }
+// }
+// console.log(count)
--- src/dream-team.js
+++ src/dream-team.js
@@ -13,11 +13,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
+function createDreamTeam(members) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+
+  let res = '';
+for(let i = 0; i < members.length; i++) {
+  
+  if(typeof members[i] === 'string'){
+    res += members[i][0];
+  }
+  // console.log(res.split('').sort().join(''));
+}
+  return res.split('').sort().join('')
 }
 
 module.exports = {
   createDreamTeam
 };
+
+let test  = [' Olivia', 1111, 'Lily', 'Oscar', true, null]
+let res = '';
+for(let i = 0; i < test.length; i++) {
+  
+  if(typeof test[i] === 'string'){
+    console.log(test[i][0])
+    res += test[i][0];
+  }
+
+  console.log(res.split('').sort().join(''));
+}
