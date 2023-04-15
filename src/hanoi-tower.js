d70a12434b81fcabaa5d67d730f4c1c0dfacb2bb
Your Name <you@example.com>
2023-04-15 14:23:51 +0300

    feat: add hanoi-tower and recursive-depth

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -3,22 +3,34 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Calculate turns number and time (in seconds) required
  * to solve puzzle
- * 
+ *
  * @param {Number} disks number of disks
  * @param {Number} turnsSpeed speed (in turns/hour)
  * @return {Object} object with props turns (number of turns)
  * and seconds (time in seconds)
  *
  * @example
- * 
+ *
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
+function calculateHanoi(disksNumber, turnsSpeed) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let countSteps = Math.pow(2, disksNumber) - 1
+  let time = 3600 / turnsSpeed
+  return {turns: countSteps, seconds: Math.floor(countSteps * time)}
 }
 
 module.exports = {
   calculateHanoi
 };
+
+
+
+//2^n - 1
+// let n = 9
+// let diskNumber = 4308
+// let countSteps = Math.pow(2, n) - 1
+// let seconds = 3600 / diskNumber
+// console.log(Math.floor(countSteps * seconds));
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -3,22 +3,59 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Implement class DepthCalculator with method calculateDepth
  * that calculates deoth of nested array
- * 
+ *
  * @example
- * 
+ *
  * const depthCalc = new DepthCalculator();
  * depthCalc.calculateDepth([1, 2, 3, 4, 5]) => 1
  * depthCalc.calculateDepth([1, 2, 3, [4, 5]]) => 2
  * depthCalc.calculateDepth([[[]]]) => 3
  *
  */
+
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
+
+  calculateDepth(arr) {
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    let count = 0
+    if(Array.isArray(arr)){
+      count++
+    }
+    for(let i = 0; i < arr.length; i++) {
+      if(Array.isArray(arr[i])){
+        arr = arr.flat()
+        count += this.calculateDepth(arr)
+        return count
+      }
+    }
+    return count
   }
 }
 
 module.exports = {
   DepthCalculator
 };
+
+
+// let test = [[1]]
+// // console.log(test.flat())
+// let count = 0
+
+
+// function getDepth(array) {
+//   if(Array.isArray(array)){
+//     count++
+//   }
+//   for(let i = 0; i < array.length; i++) {
+//     if(Array.isArray(array[i])){
+//       getDepth(array.flat())
+//     }
+//   }
+//   return count
+// }
+
+// console.log(getDepth(test));
+
+
+
